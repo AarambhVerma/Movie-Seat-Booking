302bd8159c56503593d83d0b5c25fe8100850697
AarambhVerma <aarambhverma246@gmail.com>
2020-08-07 23:21:45 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,68 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById('movie')
+
+populateUI();
+
+let ticketPrice = +movieSelect.value;
+
+//save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMoveiIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice)
+}
+
+//Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  //copy selected seats into an array
+  //Map through that array
+  //return a new array of indexes
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat))
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+//Get data from local storage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected')
+      }
+    })
+  }
+
+  const selectedMoveiIndex = localStorage.getItem('selectedMoveiIndex');
+  if (selectedMoveiIndex !== null) {
+    movieSelect.selectedIndex = selectedMoveiIndex;
+  }
+}
+
+// movie select event
+movieSelect.addEventListener('change', e => {
+  ticketPrice = +e.target.value
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
+
+
+//Seat click event
+container.addEventListener('click', e => {
+  if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+    e.target.classList.toggle('selected')
+
+    updateSelectedCount();
+  }
+})
+
+//initial count and toatal set
+updateSelectedCount();
